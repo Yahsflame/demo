cfc87246aa3108608d1df6ed8da557cf94d3ce2e
Ben Sigal <ben.sigal@gapac.com>
2025-05-01 18:05:24 -0700

    Update API endpoints and configurations

---
+++ babel.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  presets: ['next/babel'],
+  env: {
+    test: {
+      presets: [
+        ['@babel/preset-env', { targets: { node: 'current' } }],
+        '@babel/preset-typescript',
+        ['@babel/preset-react', { runtime: 'automatic' }],
+      ],
+    },
+  },
+}; 
